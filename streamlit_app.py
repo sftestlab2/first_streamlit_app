796bce52b1e7c32f7612a7157dedacd9405caff3
sftestlab2 <142623255+sftestlab2@users.noreply.github.com>
2023-08-18 14:51:26 -0500

    Update streamlit_app.py
    Added few menu things

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('testing')
+
+streamlit.title('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
